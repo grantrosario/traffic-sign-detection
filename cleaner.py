43469824b7d2ec0168d32fdb453c4be7686029ae
Grant Rosario <grantster1124@aol.com>
2017-07-31 22:28:08 -0400

    Added getData() function to perform cross-validation

--- cleaner.py
+++ cleaner.py
@@ -1,30 +1,90 @@
 import cv2
 import numpy as np
-from scipy.misc import imread, imsave, imresize
+import os
 import glob
+import pickle
+from scipy.misc import imread, imsave, imresize
+from sklearn.model_selection import train_test_split as trainTestSplit
+from sklearn.utils import shuffle
+
 
 class DataCleaner():
     """
     """
 
-    def __init__(self, not_sign_dir_path, sign_dir_path):
-        self.sign_dir = sign_dir_path
-        self.not_sign_dir = not_sign_dir_path
+    def __init__(self, notSignDirPath, signDirPath):
+        self.signDir = signDirPath
+        self.notSignDir = notSignDirPath
 
-    def clean(self):
+    def sizer(self, pathList, width, height):
 
-        not_sign_paths = glob.glob(self.not_sign_dir + '/*.png')
-        sign_paths = glob.glob(self.sign_dir + '/*/*.png')
-
-        self.sizer(not_sign_paths, 64, 64)
-        self.sizer(sign_paths, 64, 64)
-
-    def sizer(self, path_list, width, height):
-
-        for path in path_list:
+        for path in pathList:
             img = imread(path)
             img = imresize(img, (width, height))
             imsave(path, img)
 
+    def cleanData(self):
+
+        notSignPaths = glob.glob(self.notSignDir + '/*.png')
+        signPaths = glob.glob(self.signDir + '/*/*.png')
+
+        self.sizer(notSignPaths, 64, 64)
+        self.sizer(signPaths, 64, 64)
+
+    def getData(self):
+
+        dataFile = 'dataset.p'
+
+        if not os.path.isfile(dataFile):
+            print("\nAttempting to generate new data file...\n")
+
+            signFolder = '1_sign/'
+            nonSignFolder = '0_not_sign/'
+
+            if not os.path.isdir(signFolder) or not os.path.isdir(nonSignFolder):
+                print("No samples found.\nExiting.")
+                return None, None, None, None, None, None
+            else:
+                signFiles = glob.glob('{}*/*.png'.format(signFolder))
+                nonSignFiles = glob.glob('{}*.png'.format(nonSignFolder))
+
+                imageSamplesFiles = signFiles + nonSignFiles
+                y = np.concatenate((np.ones(len(signFiles)), np.zeros(len(nonSignFiles))))
+
+                imageSamplesFiles, y = shuffle(imageSamplesFiles, y)
+
+                xTrain, xTest, yTrain, yTest = trainTestSplit(imageSamplesFiles, y, test_size=0.2, random_state=42)
+
+                xTrain, xVal, yTrain, yVal = trainTestSplit(xTrain, yTrain, test_size=0.2, random_state=42)
+
+                data = {'xTrain': xTrain, 'xValidation': xVal, 'xTest': xTest,
+                        'yTrain': yTrain, 'yValidation': yVal, 'yTest': yTest}
+
+                pickle.dump(data, open(dataFile, 'wb'))
+
+                return xTrain, xVal, xTest, yTrain, yVal, yTest
+        else:
+            with open(dataFile, mode='rb') as f:
+                data = pickle.load(f)
+
+                xTrain = data['xTrain']
+                xValidation = data['xValidation']
+                xTest = data['xTest']
+                yTrain = data['yTrain']
+                yValidation = data['yValidation']
+                yTest = data['yTest']
+
+                return xTrain, xValidation, xTest, yTrain, yValidation, yTest
+
+
+
+
 clean = DataCleaner('0_not_sign', '1_sign')
-clean.clean()
+clean.cleanData()
+xTrain, xVal, xTest, yTrain, yVal, yTest = clean.getData()
+print(len(xTrain))
+print(len(xVal))
+print(len(xTest))
+print(len(yTrain))
+print(len(yVal))
+print(len(yTest))
